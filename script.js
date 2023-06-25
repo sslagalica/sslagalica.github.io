041cf4128e90a95b4c846a377131f6b2bb905ae8
Azzimihr <68514488+Azzimihr@users.noreply.github.com>
2023-06-25 20:34:56 +0200

    add fields

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-let page=document.querySelector("html").style;
-window.addEventListener("resize",()=>{
+const page=document.querySelector("html").style;
+const refresh=()=>{
   let scalex=window.innerWidth/1920;
   let scaley=window.innerHeight/1080;
   page.transform="scale("+Math.min(scalex,scaley)+")";
   page.left=Math.max((window.innerWidth-scaley*1920)/2,0)+"px";
-})
+}
+refresh();
+window.addEventListener("resize",refresh);
 let time=0;
 let total=105;
 setInterval(()=>{document.querySelector("#time div").style.height=Math.min(time*100/total,100)+"%";time+=1},1000);
