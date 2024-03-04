5e15f213a5539a293077ef5f09703fbeefca4f03
Azzimihr <68514488+Azzimihr@users.noreply.github.com>
2024-03-04 17:21:36 +0100

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-foreach (glob("*.php") as $filename) {
-  if ($filename!="index.php"){
-  echo "<a style='font-size:40px;text-decoration:none' href='$filename'>".basename($filename,".php")."</a><br><br>";
-  }
-}
-?>
